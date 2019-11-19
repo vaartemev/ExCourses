dd8474c3789d6a0fc82ff37cb06e11530a34a99e
vaartemev <im.artemev@gmail.com>
2019-11-19 10:53:55 +0300

    Second commit

--- index.js
+++ index.js
@@ -1,56 +1,28 @@
-class Calculator {
-  constructor() {
-    this.state = 0;
-  }
+const Calculator = (function() {
+  let state = 0;
 
-  getResult() {
-    return this.state;
-  }
-
-  reset() {
-    this.state = 0;
-    return `State was reseted. Now state is ${this.state}`;
-  }
-
-  add = (x = 0) => {
-    if (!this.add.value) {
-      this.add.value = this.state;
-    }
-    this.add.value += x;
-    this.add.toString = () => this.add.value;
-    this.state = this.add;
-    return this.add;
+  return {
+    add(x = 0) {
+      state += x;
+      return Calculator.add;
+    },
+    subtract(x = 0) {
+      state -= x;
+      return Calculator.subtract;
+    },
+    divide(x = 0) {
+      state /= x;
+      return Calculator.divide;
+    },
+    multiply(x = 0) {
+      state *= x;
+      return Calculator.multiply;
+    },
+    getResult() {
+      return state;
+    },
+    reset() {
+      state = 0;
+    },
   };
-
-  subtract = (x = 0) => {
-    if (!this.subtract.value) {
-      this.subtract.value = this.state;
-    }
-    this.subtract.value -= x;
-    this.subtract.toString = () => this.subtract.value;
-    this.state = this.subtract;
-    return this.subtract;
-  };
-
-  divide = (x = 0) => {
-    if (!this.divide.value) {
-      this.divide.value = this.state;
-    }
-    this.divide.value /= x;
-    this.divide.toString = () => this.divide.value;
-    this.state = this.divide;
-    return this.divide;
-  };
-
-  multiply = (x = 0) => {
-    if (!this.multiply.value) {
-      this.multiply.value = this.state;
-    }
-    this.multiply.value *= x;
-    this.multiply.toString = () => this.multiply.value;
-    this.state = this.multiply;
-    return this.multiply;
-  };
-}
-
-const calc = new Calculator();
+})();

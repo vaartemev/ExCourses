ac2892f2e0dfa8cf4c533cfbd1d94df27c19175e
vaartemev <im.artemev@gmail.com>
2020-02-06 10:32:08 +0300

    Logger shprogger

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
-var btn = document.getElementById('action');
-
-btn.addEventListener('click', throttle(letsDoIt, 1000));
-
-function throttle(fn, time) {
-  var isThrottle = false;
-  return function () {
-
-    if (!isThrottle) {
-      fn();
-      isThrottle = true;
-      setTimeout(function () {
-        isThrottle = false;
-      }, time);
+class Logger {
+  constructor(config) {
+    this.config = config;
+    this.connected = this.config.success;
+    if (Logger.instance instanceof Logger) {
+      return Logger.instance;
     }
+    Logger.instance = this;
+  }
 
-    return;
-  };
+  error(message) {
+    console.group(this.config);
+    console.error(message);
+  }
+
+  warning(message) {
+    console.warn(message);
+  }
+
+  info(message) {
+    console.info(message);
+  }
+
+  openConnection = () => {
+    if (!this.connected) {
+      this.info('Соединение установлено');
+      this.connected = true;
+    } else {
+      this.info('Соединение уже установлено');
+      console.log('Чтобы установить новое соединение остановите текущее');
+    }
+  }
+
+  closeConnection = () => {
+    if (this.connected) {
+      this.info('Соединение закрыто');
+      this.connected = false;
+    } else {
+      this.info('Никакого соединений не запущено');
+      console.log('Чтобы закрыть, надо открыть');
+    }
+  }
 }
 
-function debounce(fn, time) {
-  var timerId;
-  return function () {
-    clearTimeout(timerId);
-    timerId = setTimeout(function () {
-      fn()
-    }, time);
-  }
+const config = {
+  PORT: '5050',
+  date: new Date(),
+  success: true
 };
 
-
-function letsDoIt() {
-  console.log("It's working");
+const config1 = {
+  PORT: '5050',
+  date: new Date(),
+  success: false
 };
 
+const logger = new Logger(config1);
 
+// logger.error('Неудача');
+logger.openConnection();
+// setTimeout(logger.closeConnection, 1000);
